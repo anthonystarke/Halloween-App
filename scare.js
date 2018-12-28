679630c128f35c0f27c50a82e7142e05f70ea1c3
Anthony Starke <starke@gmail.com>
2018-12-28 20:19:48 +0000

    jscript added

--- scare.js
+++ scare.js
@@ -6,3 +6,9 @@ var minutes = time.getMinutes()
 var currentTime = hours + ":" + minutes
 
 spaceForTime.textContent = currentTime;
+
+
+var x = 0;
+function myFunction() {
+  document.getElementById("demo").innerHTML = x += 1;
+}
