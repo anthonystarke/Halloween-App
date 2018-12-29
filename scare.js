dec115cc16c8cd8b810dd52394ae602661fd2196
Anthony Starke <starke@gmail.com>
2018-12-29 15:47:15 +0000

    sound attempt

--- scare.js
+++ scare.js
@@ -1,25 +1,27 @@
-var spaceForTime = document.getElementById("time")
-var time = new Date()
-var hours = time.getHours()
-var minutes = time.getMinutes()
+var spaceForTime = document.getElementById("time");
+var time = new Date();
+var hours = time.getHours();
+var minutes = time.getMinutes();
 
-var currentTime = hours + ":" + minutes
+var currentTime = hours + ":" + minutes;
 
 spaceForTime.textContent = currentTime;
 
+window.addEventListener("scroll", scrolly);
 
-window.addEventListener("scroll", scrolly)
+function playAudio() {
+  var x = document.getElementById("myAudio");
+  x.play();
+}
 
-var x = 0;
 function scrolly() {
-    if (pageYOffset > 200) {
-      // document.getElementById("demo").innerHTML = x += 1;
+
+    if (pageYOffset > 100) {
       document.getElementById("wrapper").style.display = "none";
       document.getElementById("main").style.display = "none";
       document.getElementById("scaryImage").style.display = "inline";
       // document.getElementByTag('body').background-image: "";
       document.body.style.backgroundImage = "url('./images/empty.jpg')";
-
-
+      document.body.style.backgroundColor = "black";
     }
 }
